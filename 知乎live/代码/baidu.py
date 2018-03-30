133e19a5e6a76511549d8d51f4c9e6ff1c78f437
亢超 <kangchaolove2009@163.com>
2018-03-30 13:41:55 +0800

    测试一个百度人脸识别的deamon

--- 知乎live/代码/baidu.py
+++ 知乎live/代码/baidu.py
@@ -1,33 +1,44 @@
-# -*- coding: utf-8 -*-
-import sys, urllib, urllib2, json
+# encoding:utf-8
+import base64
+import urllib
+import urllib2
+import json;
 
-url = 'http://apis.baidu.com/idl_baidu/pornfilter/pornfilter'
+'''
+人脸探测
+'''
 
-data = {}
-data[''] = {
-    "params": [
-        {
-            "image": "/9j/4AAQSkZJRgABAQEAYABgAAD/2wBDABMNDxEPDBMREBEWFRMXHTAfHRsbHTsqLSMwRj5KSUU+RENNV29eTVJpU0NEYYRiaXN3fX59S12Jkoh5kW96fXj/2wBDARUWFh0ZHTkfHzl4UERQeHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHj/wAARCAAfACEDAREAAhEBAxEB/8QAGAABAQEBAQAAAAAAAAAAAAAAAAQDBQb/xAAjEAACAgICAgEFAAAAAAAAAAABAgADBBESIRMxBSIyQXGB/8QAFAEBAAAAAAAAAAAAAAAAAAAAAP/EABQRAQAAAAAAAAAAAAAAAAAAAAD/2gAMAwEAAhEDEQA/APawEBAQEBAgy8i8ZTVV3UY6V1eU2XoWDDZB19S646Gz39w9fkKsW1r8Wm2yo1PYis1be0JG9H9QNYCAgc35Cl3yuVuJZl0cB41rZQa32dt2y6OuOiOxo61vsLcVblxaVyXD3hFFjL6La7I/sDWAgICAgICB/9k=",
-            "versionnum": "1.0",
-            "logid": 1,
-            "cmdid": "cmdid",
-            "appid": "appid",
-            "clientip": "10.23.34.5",
-            "type": "type"
-        }
-    ],
-    "jsonrpc": "2.0",
-    "method": "classify",
-    "id": 12345
-}
 
-decoded_data = urllib.urlencode(data)
-req = urllib2.Request(url, data = decoded_data)
+token = "";
+host = 'https://aip.baidubce.com/oauth/2.0/token?grant_type=client_credentials&client_id=GWAy6GnFrwljCDBTxQAoIZCQ&client_secret=iif31FfMrGZEFcihB4H40VHrg8B5w0ku'
+request = urllib2.Request(host)
+request.add_header('Content-Type', 'application/json; charset=UTF-8')
+response = urllib2.urlopen(request)
+content = response.read()
+if (content):
+    ret = json.loads(content);
+    for a,b in ret.items():
+        if a == "access_token":
+            token = b;
 
-req.add_header("Content-Type", "application/x-www-form-urlencoded")
-req.add_header("apikey", "657c2798356b9c1c7652beab927814c7")
 
-resp = urllib2.urlopen(req)
-content = resp.read()
-if(content):
-    print(content)
+print token;
+
+
+request_url = "https://aip.baidubce.com/rest/2.0/face/v1/detect"
+
+# 二进制方式打开图片文件
+f = open('C:\\Users\\Think\\Desktop\\333.jpg', 'rb')
+img = base64.b64encode(f.read())
+
+params = {"face_fields":"age,beauty,expression,faceshape,gender,glasses,landmark,race,qualities","image":img,"max_face_num":12}
+params = urllib.urlencode(params)
+
+access_token = token;
+request_url = request_url + "?access_token=" + access_token
+request = urllib2.Request(url=request_url, data=params)
+request.add_header('Content-Type', 'application/x-www-form-urlencoded')
+response = urllib2.urlopen(request)
+content = response.read()
+if content:
+    print content
